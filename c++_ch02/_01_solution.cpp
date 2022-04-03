5265593b1a22a095e2f99673e3e0fb1b523a8432
eunseong, park <96706954+esmeii@users.noreply.github.com>
2022-04-03 09:17:35 +0900

    Create _01_solution.cpp

---
+++ c++_ch02/_01_solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void IncreOne(int &num){
+  num++;
+}
+void InverSign(int &num){
+  num*=-1;
+}
+void main(void){
+   int val = 20;
+  IncreOne(val);
+  cout<<val<<endl
+    InverSign(val);
+  cout<<val<<endl;
+  return 0;
+}
